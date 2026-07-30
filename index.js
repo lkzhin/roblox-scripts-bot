a62a38230dcd80a1db64f48028a92c4d57b71896
lkzhin <lkzhinroyale@gmail.com>
2026-07-30 06:47:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,25 @@ const { Client, GatewayIntentBits } = require("discord.js");
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
   ]
 });
 
 client.once("ready", () => {
   console.log(`${client.user.tag} está online!`);
+});
+
+client.on("messageCreate", (message) => {
+  if (message.author.bot) return;
+
+  if (message.content === "!ping") {
+    message.reply("🏓 Pong!");
+  }
+
+  if (message.content === "!delta") {
+    message.reply("🔗 Delta Executor: https://deltaexploits.gg/");
+  }
 });
 
 client.login(process.env.TOKEN);
